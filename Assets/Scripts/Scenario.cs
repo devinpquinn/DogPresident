9443930493ed0b40a075ed58f21264e198e5e433
Devin <devinpquinn4@gmail.com>
2025-04-13 15:24:44 -0400

    scenario, response, and attributes

--- Assets/Scripts/Scenario.cs
+++ Assets/Scripts/Scenario.cs
@@ -1,11 +1,39 @@
 using UnityEngine;
 using System.Collections.Generic;
+using NaughtyAttributes;
 
-[CreateAssetMenu(fileName = "New Scenario", menuName = "Presidential/Scenario", order = 0)]
+[CreateAssetMenu(fileName = "NewScenario", menuName = "DogPresident/Scenario", order = 0)]
 public class Scenario : ScriptableObject
 {
     [TextArea]
     public string promptText;
 
-    public List<string> responses = new List<string>();
+    public List<Response> responses = new List<Response>();
+}
+
+[System.Serializable]
+public class Response
+{
+    [TextArea]
+    public string responseText;
+
+    public ApprovalRatingEffect approvalEffect = ApprovalRatingEffect.NA;
+
+    [ShowIf("HasApprovalEffect")]
+    public string headline;
+
+    // This method is used by NaughtyAttributes to determine whether to show the headline
+    public bool HasApprovalEffect()
+    {
+        return approvalEffect != ApprovalRatingEffect.NA;
+    }
+}
+
+public enum ApprovalRatingEffect
+{
+    NA,
+    PositiveLarge,
+    PositiveSmall,
+    NegativeSmall,
+    NegativeLarge
 }
