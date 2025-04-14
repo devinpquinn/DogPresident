421bb9d83b2da5c0072721f2cef1a7aa29575ddd
Devin <devinpquinn4@gmail.com>
2025-04-13 22:15:05 -0400

    scenario manager

---
+++ Assets/Scripts/ScenarioManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ScenarioManager : MonoBehaviour
+{
+    private List<Scenario> scenarioPool = new List<Scenario>();
+    private int currentScenarioIndex = -1;
+    private Scenario currentScenario;
+
+    public delegate void OnScenarioChanged(Scenario newScenario);
+    public event OnScenarioChanged onScenarioChanged;
+
+    public delegate void OnResponsePlayed(Response response);
+    public event OnResponsePlayed onResponsePlayed;
+
+    void Start()
+    {
+        LoadAndShuffleScenarios();
+        AdvanceToNextScenario();
+    }
+
+    private void LoadAndShuffleScenarios()
+    {
+        Scenario[] loadedScenarios = Resources.LoadAll<Scenario>("Scenarios"); // "Resources/Scenarios"
+        scenarioPool = new List<Scenario>(loadedScenarios);
+        ShuffleScenarioPool();
+    }
+
+    private void ShuffleScenarioPool()
+    {
+        for (int i = 0; i < scenarioPool.Count; i++)
+        {
+            Scenario temp = scenarioPool[i];
+            int randomIndex = Random.Range(i, scenarioPool.Count);
+            scenarioPool[i] = scenarioPool[randomIndex];
+            scenarioPool[randomIndex] = temp;
+        }
+
+        currentScenarioIndex = -1;
+    }
+
+    public void AdvanceToNextScenario()
+    {
+        currentScenarioIndex++;
+
+        if (currentScenarioIndex >= scenarioPool.Count)
+        {
+            ShuffleScenarioPool();
+            currentScenarioIndex = 0;
+        }
+
+        currentScenario = scenarioPool[currentScenarioIndex];
+        onScenarioChanged?.Invoke(currentScenario);
+    }
+
+    public void PlayResponse(int responseIndex)
+    {
+        if (currentScenario == null || responseIndex < 0 || responseIndex >= currentScenario.responses.Count)
+        {
+            Debug.LogWarning("Invalid response index or no current scenario.");
+            return;
+        }
+
+        Response response = currentScenario.responses[responseIndex];
+        Debug.Log("Player selected response: " + response.responseText);
+
+        onResponsePlayed?.Invoke(response);
+
+        if (response.approvalEffect != ApprovalRatingEffect.NA)
+        {
+            Debug.Log("Headline: " + response.headline);
+            AdvanceToNextScenario();
+        }
+        else
+        {
+            Debug.Log("Scenario continues.");
+            // Do nothing; player may press another button
+        }
+    }
+}
